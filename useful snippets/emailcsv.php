03811812c926924d43a1e47c9b5f7984ff033a2c
Phil <Phil@Phil-PC.lan>
2016-05-31 20:10:19 +0100

    snippet

--- useful snippets/emailcsv.php
+++ useful snippets/emailcsv.php
@@ -1,7 +1,4 @@
 <?php
-/***** LICENSE GOES HERE *****/
-include_once 'functions_atl.php';
-
 // array of email addresses
 $to = array();
 
@@ -19,14 +16,12 @@ function array2csv(array &$array)
 }
 
 function getData(){
-        $start = date('Y-m-d',strtotime("-1 days")) . ' 00:00:00';
-        $end = date('Y-m-d',strtotime("-1 days")) . ' 23:59:59';
         $return = array();
         $dbDetails = readConfig();
-        $table = 'vicidial_closer_log';
-        $query = "SELECT closecallid, lead_id, list_id, campaign_id, call_date, start_epoch, end_epoch, length_in_sec, `status`, phone_code, phone_number, user, comments, processed, queue_seconds, user_group, xfercallid, term_reason, uniqueid, agent_only, queue_position, called_count ";
-        $query .= " FROM vicidial_closer_log WHERE call_date > '$start' AND call_date < '$end'";
-        $conn = new mysqli($dbDetails[VARDB_server], $dbDetails[VARDB_user], $dbDetails[VARDB_pass], $dbDetails[VARDB_database]);
+        $table = '';
+        $query = "SELECT ";
+        $query .= "FROM ";
+        $conn = new mysqli($dbDetails[server], $dbDetails[user], $dbDetails[pass], $dbDetails[database]);
         $result = $conn->query($query);
         while ($row = $result->fetch_assoc()){
                 $return[] = $row;
@@ -35,7 +30,7 @@ function getData(){
         return $return;
 }
 
-function emailCsv($csv_path, $body, $to = 'phil@antheus.co.uk', $subject = 'CSV Report data', $from = 'support@antheus.co.uk') {
+function emailCsv($csv_path, $body, $to = 'phil@email.co.uk', $subject = 'CSV Report data', $from = 'support@email.co.uk') {
         $logopt = '';
         $subject = escapeshellarg($subject);
         $attachment_path = escapeshellarg($csv_path);
@@ -50,7 +45,7 @@ function emailCsv($csv_path, $body, $to = 'phil@antheus.co.uk', $subject = 'CSV 
 
 $data = getData();
 
-$body = "Please see attached CSV for inbound data for " . date('Y-m-d', strtotime("-1 days"));
+$body = "Please see attached CSV for " . date('Y-m-d', strtotime("-1 days"));
 array2csv($data);
 
 emailCsv("/tmp/report_".date('Y-m-d', strtotime("-1 days")).".csv", $body, $to);
