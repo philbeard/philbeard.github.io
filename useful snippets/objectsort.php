fc9920849beb3c40175d0727d3677dd87b55a95b
phil <phil@antheus.co.uk>
2016-07-07 14:34:24 +0100

    sort now with reverse order

--- useful snippets/objectsort.php
+++ useful snippets/objectsort.php
@@ -1,8 +1,15 @@
 <?php
-/***** LICENSE GOES HERE *****/
-$callback = function ($a, $b) use ($orderBy) {
+$sortOrder = $reportTool->getReverse() ? 'DESC' : 'ASC';
+$orderBy = $reportTool->getSortcol() ? $reportTool->getSortcol() : 'Agent';
+// object sorting callback function
+$callback = function ($a, $b) use ($orderBy , $sortOrder) {
 	$orderBy = 'get'. $orderBy;
-	if($a->{$orderBy}() == $b->{$orderBy}()){ return 0 ; }
-	return ($a->{$orderBy}() < $b->{$orderBy}()) ? -1 : 1;
+	if ($sortOrder == 'DESC') {
+		if($a->{$orderBy}() == $b->{$orderBy}()){ return 0 ; }
+		return ($a->{$orderBy}() < $b->{$orderBy}()) ? -1 : 1;
+	} else {
+		if($b->{$orderBy}() == $a->{$orderBy}()){ return 0 ; }
+		return ($b->{$orderBy}() < $a->{$orderBy}()) ? -1 : 1;
+	}
 };
 usort($agentPerformances, $callback);
